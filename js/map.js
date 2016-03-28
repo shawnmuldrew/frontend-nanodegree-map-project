17da5aa4b929cb503b428095e1e5f4556914b816
Shawn Muldrew <shawnmuldrew@gmail.com>
2016-03-28 14:18:04 -0600

    add infowindow on list click. Add makrer clicking

--- js/map.js
+++ js/map.js
@@ -5,24 +5,25 @@ var red = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 var green = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
 
 var stationList = [
-    {title: 'McKernan/Belgravia LRT Station',latitude: 53.5105176626514,longitude: -113.525670719744,address: '114 Street 76 Avenue NW',icon: blue},
-    {title: 'Bay/Enterprise Square LRT Station',latitude: 53.5408338367352,longitude: -113.500257591202,address: '104 Street Jasper Avenue NW',icon: blue},
-    {title: 'Belvedere LRT Station',latitude: 53.5881416762881,longitude: -113.435304152645,address: '58 Street 129 Avenue NW',icon: blue},
-    {title: 'Central LRT Station',latitude: 53.540933109878,longitude: -113.495220359409,address: '101 Street Jasper Avenue NW',icon: blue},
-    {title: 'Century Park LRT Station',latitude: 53.4572535293499,longitude: -113.515815605592,address: '111 Street 23 Avenue NW',icon: blue},
-    {title: 'Churchill LRT Station',latitude: 53.5461618022682,longitude: -113.489906967631,address: '99 Street 102 Avenue NW',icon: blue},
-    {title: 'Clareview LRT Station',latitude: 53.6015653908153,longitude: -113.410802221039,address: '100 Clareview Transit Station NW',icon: blue},
-    {title: 'Coliseum LRT Station',latitude: 53.5704958297835,longitude: -113.459210264724,address: '7628 118 Avenue NW',icon: blue},
-    {title: 'Corona LRT Station',latitude: 53.5408294776932,longitude: -113.504000779089,address: '107 Street Jasper Avenue NW',icon: blue},
-    {title: 'Grandin LRT Station',latitude: 53.5361496789029,longitude: -113.504462742382,address: '110 Street 98 Avenue NW',icon: blue},
-    {title: 'Health Sciences/Jubilee LRT Station',latitude: 53.520384915746,longitude: -113.52581684413,address: '114 Street 83 Avenue NW',icon: blue},
-    {title: 'South Campus/Fort Edmonton LRT Station',latitude: 53.5025223521144,longitude: -113.528518164187,address: '11457 68 Avenue NW',icon: blue},
-    {title: 'Southgate LRT Station',latitude: 53.4853395036959,longitude: -113.516896977959,address: '111 Street 51 Avenue NW',icon: blue},
-    {title: 'Stadium LRT Station',latitude: 53.5595244773473,longitude: -113.471393915512,address: '100 Stadium Transit Station NW',icon: blue},
-    {title: 'University LRT Station',latitude: 53.5248259947652,longitude: -113.521801205501,address: '100 University Station NW',icon: blue}
+    {title: 'McKernan/Belgravia LRT Station',latitude: 53.5105176626514,longitude: -113.525670719744,address: '114 Street 76 Avenue NW',icon: blue,order: 12},
+    {title: 'Bay/Enterprise Square LRT Station',latitude: 53.5408338367352,longitude: -113.500257591202,address: '104 Street Jasper Avenue NW',icon: blue,order: 7},
+    {title: 'Belvedere LRT Station',latitude: 53.5881416762881,longitude: -113.435304152645,address: '58 Street 129 Avenue NW',icon: blue,order: 2},
+    {title: 'Central LRT Station',latitude: 53.540933109878,longitude: -113.495220359409,address: '101 Street Jasper Avenue NW',icon: blue,order: 6},
+    {title: 'Century Park LRT Station',latitude: 53.4572535293499,longitude: -113.515815605592,address: '111 Street 23 Avenue NW',icon: blue,order: 15},
+    {title: 'Churchill LRT Station',latitude: 53.5461618022682,longitude: -113.489906967631,address: '99 Street 102 Avenue NW',icon: blue,order: 5},
+    {title: 'Clareview LRT Station',latitude: 53.6015653908153,longitude: -113.410802221039,address: '100 Clareview Transit Station NW',icon: blue,order: 1},
+    {title: 'Coliseum LRT Station',latitude: 53.5704958297835,longitude: -113.459210264724,address: '7628 118 Avenue NW',icon: blue,order: 3},
+    {title: 'Corona LRT Station',latitude: 53.5408294776932,longitude: -113.504000779089,address: '107 Street Jasper Avenue NW',icon: blue,order: 8},
+    {title: 'Grandin LRT Station',latitude: 53.5361496789029,longitude: -113.504462742382,address: '110 Street 98 Avenue NW',icon: blue,order: 9},
+    {title: 'Health Sciences/Jubilee LRT Station',latitude: 53.520384915746,longitude: -113.52581684413,address: '114 Street 83 Avenue NW',icon: blue,order: 11},
+    {title: 'South Campus/Fort Edmonton LRT Station',latitude: 53.5025223521144,longitude: -113.528518164187,address: '11457 68 Avenue NW',icon: blue,order: 13},
+    {title: 'Southgate LRT Station',latitude: 53.4853395036959,longitude: -113.516896977959,address: '111 Street 51 Avenue NW',icon: blue,order: 14},
+    {title: 'Stadium LRT Station',latitude: 53.5595244773473,longitude: -113.471393915512,address: '100 Stadium Transit Station NW',icon: blue,order: 4},
+    {title: 'University LRT Station',latitude: 53.5248259947652,longitude: -113.521801205501,address: '100 University Station NW',icon: blue,order: 10}
   ];
 
 var markers = [];
+var infowindows = [];
 
 var station = function(data) {
   this.title = ko.observable(data.title);
@@ -40,33 +41,40 @@ var ViewModel = function() {
 
   stationList.forEach(function(stationItem){
     self.stationLocation.push( new station(stationItem) );
-    addMarker({lat: stationItem.latitude, lng: stationItem.longitude},stationItem.title,stationItem.icon);
+    addMarker(stationItem);
   });
 
   this.currentStation = ko.observable(this.stationLocation()[0] );
 
   this.switchStation = function(clickedStation) {
     self.currentStation(clickedStation);
-    console.log(self.currentStation().title);
-//    setMapOnAll(map);
     setCurrentStation(map,self.currentStation().title,blue,green);
   };
 
   // Adds a marker to the map and push to the array.
-  function addMarker(location,title,icon) {
+  // Adds station information to infowindow and pushes to the array.
+  function addMarker(stationItem) {
     var marker = new google.maps.Marker({
-      position: location,
-      title: title,
-      icon: icon,
+      position: {lat: stationItem.latitude, lng: stationItem.longitude},
+      title: stationItem.title,
+      icon: stationItem.icon,
       map: map
     });
+    marker.addListener('click', function() {
+      self.currentStation(stationItem);
+      setCurrentStation(map,self.currentStation().title,blue,green);
+    });
+    var infowindow = new google.maps.InfoWindow({
+    content: '<p><b>'+stationItem.title+'</b></p>'+'<p>'+stationItem.address+'</p>'
+    });
     markers.push(marker);
+    infowindows.push(infowindow);
   }
 
   // Set markers on map for all in the array.
-  function setMapOnAll(map) {
+  function setAllMarkersOnMap(map,colour) {
     for (var i = 0; i < markers.length; i++) {
-      markers[i].setIcon(green);
+      markers[i].setIcon(colour);
       markers[i].setMap(map);
     }
   }
@@ -76,8 +84,10 @@ var ViewModel = function() {
     for (var i = 0; i < markers.length; i++) {
       if (markers[i].title == currentStationTitle) {
         markers[i].setIcon(currentColor);
+        infowindows[i].open(map,markers[i]);
       } else {
         markers[i].setIcon(baseColor);
+        infowindows[i].close(map,markers[i]);
       }
       markers[i].setMap(map);
     }
