0a5062703306e1a160587d62f09312adf3e4e76a
Shawn Muldrew <shawnmuldrew@gmail.com>
2016-04-05 11:29:43 -0600

    add north and south stops to data. Add dynamically generated API based on stop_id

--- js/map.js
+++ js/map.js
@@ -1,26 +1,26 @@
 // DATA MODEL
 
-var stationList = [
-    {title: 'McKernan/Belgravia LRT Station',latitude: 53.5105176626514,longitude: -113.525670719744,address: '114 Street 76 Avenue NW',icon: blue,order: 12},
-    {title: 'Bay/Enterprise Square LRT Station',latitude: 53.5408338367352,longitude: -113.500257591202,address: '104 Street Jasper Avenue NW',icon: blue,order: 7},
-    {title: 'Belvedere LRT Station',latitude: 53.5881416762881,longitude: -113.435304152645,address: '58 Street 129 Avenue NW',icon: blue,order: 2},
-    {title: 'Central LRT Station',latitude: 53.540933109878,longitude: -113.495220359409,address: '101 Street Jasper Avenue NW',icon: blue,order: 6},
-    {title: 'Century Park LRT Station',latitude: 53.4572535293499,longitude: -113.515815605592,address: '111 Street 23 Avenue NW',icon: blue,order: 15},
-    {title: 'Churchill LRT Station',latitude: 53.5461618022682,longitude: -113.489906967631,address: '99 Street 102 Avenue NW',icon: blue,order: 5},
-    {title: 'Clareview LRT Station',latitude: 53.6015653908153,longitude: -113.410802221039,address: '100 Clareview Transit Station NW',icon: blue,order: 1},
-    {title: 'Coliseum LRT Station',latitude: 53.5704958297835,longitude: -113.459210264724,address: '7628 118 Avenue NW',icon: blue,order: 3},
-    {title: 'Corona LRT Station',latitude: 53.5408294776932,longitude: -113.504000779089,address: '107 Street Jasper Avenue NW',icon: blue,order: 8},
-    {title: 'Grandin LRT Station',latitude: 53.5361496789029,longitude: -113.504462742382,address: '110 Street 98 Avenue NW',icon: blue,order: 9},
-    {title: 'Health Sciences/Jubilee LRT Station',latitude: 53.520384915746,longitude: -113.52581684413,address: '114 Street 83 Avenue NW',icon: blue,order: 11},
-    {title: 'South Campus/Fort Edmonton LRT Station',latitude: 53.5025223521144,longitude: -113.528518164187,address: '11457 68 Avenue NW',icon: blue,order: 13},
-    {title: 'Southgate LRT Station',latitude: 53.4853395036959,longitude: -113.516896977959,address: '111 Street 51 Avenue NW',icon: blue,order: 14},
-    {title: 'Stadium LRT Station',latitude: 53.5595244773473,longitude: -113.471393915512,address: '100 Stadium Transit Station NW',icon: blue,order: 4},
-    {title: 'University LRT Station',latitude: 53.5248259947652,longitude: -113.521801205501,address: '100 University Station NW',icon: blue,order: 10}
-  ];
-
 var blue = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png';
 var red = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png';
 var green = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png';
+
+var stationList = [
+    {title: 'McKernan/Belgravia LRT Station',latitude: 53.5105176626514,longitude: -113.525670719744,address: '114 Street 76 Avenue NW',icon: blue,order: 12, northStop: '9982', southStop: '9981'},
+    {title: 'Bay/Enterprise Square LRT Station',latitude: 53.5408338367352,longitude: -113.500257591202,address: '104 Street Jasper Avenue NW',icon: blue,order: 7, northStop: '1985', southStop: '1774'},
+    {title: 'Belvedere LRT Station',latitude: 53.5881416762881,longitude: -113.435304152645,address: '58 Street 129 Avenue NW',icon: blue,order: 2, northStop: '7830', southStop: '7692'},
+    {title: 'Central LRT Station',latitude: 53.540933109878,longitude: -113.495220359409,address: '101 Street Jasper Avenue NW',icon: blue,order: 6, northStop: '1863', southStop: '1935'},
+    {title: 'Century Park LRT Station',latitude: 53.4572535293499,longitude: -113.515815605592,address: '111 Street 23 Avenue NW',icon: blue,order: 15, northStop: '4982', southStop: '4982'},
+    {title: 'Churchill LRT Station',latitude: 53.5461618022682,longitude: -113.489906967631,address: '99 Street 102 Avenue NW',icon: blue,order: 5, northStop: '1691', southStop: '1876'},
+    {title: 'Clareview LRT Station',latitude: 53.6015653908153,longitude: -113.410802221039,address: '100 Clareview Transit Station NW',icon: blue,order: 1, northStop: '7977', southStop: '7977'},
+    {title: 'Coliseum LRT Station',latitude: 53.5704958297835,longitude: -113.459210264724,address: '7628 118 Avenue NW',icon: blue,order: 3, northStop: '1742', southStop: '1889'},
+    {title: 'Corona LRT Station',latitude: 53.5408294776932,longitude: -113.504000779089,address: '107 Street Jasper Avenue NW',icon: blue,order: 8, northStop: '1926', southStop: '1891'},
+    {title: 'Grandin LRT Station',latitude: 53.5361496789029,longitude: -113.504462742382,address: '110 Street 98 Avenue NW',icon: blue,order: 9, northStop: '1754', southStop: '1925'},
+    {title: 'Health Sciences/Jubilee LRT Station',latitude: 53.520384915746,longitude: -113.52581684413,address: '114 Street 83 Avenue NW',icon: blue,order: 11, northStop: '2014', southStop: '2019'},
+    {title: 'South Campus/Fort Edmonton LRT Station',latitude: 53.5025223521144,longitude: -113.528518164187,address: '11457 68 Avenue NW',icon: blue,order: 13, northStop: '2116', southStop: '2115'},
+    {title: 'Southgate LRT Station',latitude: 53.4853395036959,longitude: -113.516896977959,address: '111 Street 51 Avenue NW',icon: blue,order: 14, northStop: '2114', southStop: '2113'},
+    {title: 'Stadium LRT Station',latitude: 53.5595244773473,longitude: -113.471393915512,address: '100 Stadium Transit Station NW',icon: blue,order: 4, northStop: '1981', southStop: '1723'},
+    {title: 'University LRT Station',latitude: 53.5248259947652,longitude: -113.521801205501,address: '100 University Station NW',icon: blue,order: 10, northStop: '2969', southStop: '2316'}
+  ];
 
 var markers = [];
 var infowindows = [];
@@ -32,6 +32,8 @@ var Station = function(data) {
   this.address = ko.observable(data.address);
   this.icon = ko.observable(data.icon);
   this.order = ko.observable(data.order);
+  this.northStop = ko.observable(data.northStop);
+  this.southStop = ko.observable(data.southStop);
 };
 
 // VIEW MODEL
@@ -44,10 +46,11 @@ var ViewModel = function() {
   // Load observable array
   stationList.forEach(function(stationItem){
     self.stationLocation.push( new Station(stationItem) );
-    addMarker(stationItem);
+    //addMarker(stationItem);
+    addMarker(self.stationLocation()[self.stationLocation().length-1]);
   });
 
-  // Added because it's not included in knockout-3.4.0.js file
+  // Added because it's not included in the minified knockout-3.4.0.js file
   ko.utils.stringStartsWith = function (string, startsWith) {         
           string = string || "";
           if (startsWith.length > string.length)
@@ -80,17 +83,18 @@ var ViewModel = function() {
   // Adds station information to infowindow and pushes to the array.
   function addMarker(stationItem) {
     var marker = new google.maps.Marker({
-      position: {lat: stationItem.latitude, lng: stationItem.longitude},
-      title: stationItem.title,
-      icon: stationItem.icon,
+      position: {lat: stationItem.latitude(), lng: stationItem.longitude()},
+      title: stationItem.title(),
+      icon: stationItem.icon(),
       map: map
     });
     marker.addListener('click', function() {
       self.currentStation(stationItem);
-      setCurrentStation(map,self.currentStation().title,blue,green);
+      setCurrentStation(map,self.currentStation().title(),blue,green);
     });
     var infowindow = new google.maps.InfoWindow({
-    content: '<p><b>'+stationItem.title+'</b></p>'+'<p>'+stationItem.address+'</p>'
+    content: ''
+    //content: '<p><b>'+stationItem.title+'</b></p>'+'<p>'+stationItem.address+'</p>'
     });
     markers.push(marker);
     infowindows.push(infowindow);
@@ -109,8 +113,13 @@ var ViewModel = function() {
     for (var i = 0; i < markers.length; i++) {
       if (markers[i].title == currentStationTitle) {
         markers[i].setIcon(currentColor);
-      console.log('nextThree: '+getNextTrains());
-        infowindows[i].setContent(infowindows[i].getContent()+getNextTrains());
+        console.log('northStop: '+self.currentStation().northStop());
+        console.log('southStop: '+self.currentStation().southStop());
+        var stationInfo = '<p><b>'+self.currentStation().title()+'</b></p>'+'<p>'+self.currentStation().address()+'</p>';
+        var northStopTimes = getNextTrains('north',self.currentStation().northStop());
+        var southStopTimes = getNextTrains('south',self.currentStation().southStop());
+        //infowindows[i].setContent('<p><b>'+self.currentStation().title()+'</b></p>'+'<p>'+self.currentStation().address()+'</p>'+getNextTrains());
+        infowindows[i].setContent(stationInfo+northStopTimes+southStopTimes);
         infowindows[i].open(map,markers[i]);
       } else {
         markers[i].setIcon(baseColor);
@@ -121,27 +130,43 @@ var ViewModel = function() {
   }
 
   // ETS JSON request
-  function getNextTrains() {
+  function getNextTrains(direction,stop_id) {
     var nextThree; 
-    var etsUrl = 'https://data.edmonton.ca/resource/xeux-ngrz.json?$query=SELECT%20arrival_time_2%20WHERE%20stop_id=%222316%22%20AND%20stop_headsign=%22Century%20Park%22%20GROUP%20BY%20arrival_time_2%20ORDER%20BY%20arrival_time_2%20ASC';
+    if (direction == 'south') {
+      var stop_headsign = 'Century%20Park';
+    } else {
+      var stop_headsign = 'Clareview';
+    }
+    console.log('stop_id: '+stop_id+' stop_headsign: '+stop_headsign);
+    //var etsUrl = 'https://data.edmonton.ca/resource/xeux-ngrz.json?$query=SELECT%20arrival_time_2%20WHERE%20stop_id=%222316%22%20AND%20stop_headsign=%22Century%20Park%22%20GROUP%20BY%20arrival_time_2%20ORDER%20BY%20arrival_time_2%20ASC';
+    var etsUrl = 'https://data.edmonton.ca/resource/xeux-ngrz.json?$query=SELECT%20arrival_time_2%20WHERE%20stop_id=%22'+stop_id+'%22%20AND%20stop_headsign=%22'+stop_headsign+'%22%20GROUP%20BY%20arrival_time_2%20ORDER%20BY%20arrival_time_2%20ASC';
+    console.log('url: '+etsUrl);
     $.ajax({
       url: etsUrl,
       dataType: 'json',
       async: false,
       success: function(data) {
-      arrivalTimes = data;
-      console.log(arrivalTimes.length);
-      console.log(timeNow());
-      var i = 0;
-      var arrivalTime = arrivalTimes[i].arrival_time_2;
-      while (timeNow() > arrivalTime) {
-        i++;
-        arrivalTime = arrivalTimes[i].arrival_time_2;
-        if (i > arrivalTimes.length) {
-          break;
+        arrivalTimes = data;
+        console.log(arrivalTimes.length);
+        console.log(timeNow());
+        var i = 0;
+        if (arrivalTimes.length > 0) {
+          var arrivalTime = arrivalTimes[i].arrival_time_2;
+          while (timeNow() > arrivalTime) {
+            i++;
+            arrivalTime = arrivalTimes[i].arrival_time_2;
+            if (i > arrivalTimes.length) {
+              break;
+            }
+          }
+          if (direction == 'south') {
+            nextThree = '<p> South Trains: <br>'+arrivalTimes[i].arrival_time_2+'<br>'+arrivalTimes[i+1].arrival_time_2+'<br>'+arrivalTimes[i+2].arrival_time_2+'</p>';
+          } else {
+            nextThree = '<p> North Trains: <br>'+arrivalTimes[i].arrival_time_2+'<br>'+arrivalTimes[i+1].arrival_time_2+'<br>'+arrivalTimes[i+2].arrival_time_2+'</p>';
+          }
+        } else {
+            nextThree = 'No Train Times Available';
         }
-      }
-      nextThree = '<p> Next Three Trains: <br>'+arrivalTimes[i].arrival_time_2+'<br>'+arrivalTimes[i+1].arrival_time_2+'<br>'+arrivalTimes[i+2].arrival_time_2+'</p>';
       }
     });
     console.log('Test: '+nextThree);
